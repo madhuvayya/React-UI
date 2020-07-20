ef7a37ec043cfc36eb789e2015acf3ee344348e4
madhuvayya <madhu.vayya@gmail.com>
2020-07-20 10:28:01 +0530

    Added content in the Subscription component

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -2,17 +2,58 @@ import React, { Component } from 'react';
 
 class Subscription extends Component {
     render() {
-        return  <div class="subscription">
-        <div class="text">
-            <h2>Pricing</h2>
-            <p class="text">Quickly build an effective pricing table for your potential customers with this layout.
-            It's built with default Material-UI components with little customization.
-            </p>
+        return <div class="subscription">
+            <div class="text">
+                <h2>Pricing</h2>
+                <p class="text">Quickly build an effective pricing table for your potential customers with this layout.
+                It's built with default Material-UI components with little customization.
+                </p>
+            </div>
+            <div class="pricing">
+                <div class="free">
+                    <div class="subs-title">
+                        Free
+                    </div>
+                    <div class="subs-body">
+                        <ul>
+                            <li>10 users included</li>
+                            <li>2GB of storage</li>
+                            <li>Help center access</li>
+                            <li>Email support</li>
+                        </ul>
+                        <button>SIGN UP FOR FREE</button>
+                </div>
+                <div class="pro">
+                <div class="subs-title">
+                        Free
+                    </div>
+                    <div class="subs-body">
+                        <ul>
+                            <li>20 users included</li>
+                            <li>10GB of storage</li>
+                            <li>Help center access</li>
+                            <li>Priority eamil support</li>
+                        </ul>
+                        <button>GET STARTED</button>
+                    </div>
+                </div>
+                <div class="enterprise">
+                    <div class="subs-title">
+                        Free
+                    </div>
+                    <div class="subs-body">
+                        <ul>
+                            <li>50 users included</li>
+                            <li>30GB of storage</li>
+                            <li>Help center access</li>
+                            <li>Phone & email support</li>
+                        </ul>
+                        <button>CONTACT US</button>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="pricing">
-            pricing
-        </div>
-    </div>
+    </div>    
     }; 
 }
 
