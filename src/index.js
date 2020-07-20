b57a8fa5938ff00398eeaa8c4262f786f30e4955
madhuvayya <madhu.vayya@gmail.com>
2020-07-20 08:40:40 +0530

    Initialized project setup

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Header from './components/components';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );
