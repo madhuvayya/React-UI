46c23ba73123f10b8c6d3b215d1849b7ded9f5a6
madhuvayya <madhu.vayya@gmail.com>
2020-07-20 09:02:28 +0530

    Added header component

--- src/components/components.jsx
+++ src/components/components.jsx
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 
 class Header extends Component {
     render() {
-        return <h1>Hello World!</h1>
-    } 
+        return  <div class="header">
+        <div class="company-name">
+            Company name
+        </div>
+        <div class="options">
+            <div><a href="#">FEATURES</a></div>
+            <div><a href="#">ENTERPRISE</a></div>
+            <div><a href="#">SUPPORT</a></div>    
+            <div><button class="login-button">LOGIN</button></div>
+        </div>    
+    </div>
+    }; 
 }
 
 export default Header;
